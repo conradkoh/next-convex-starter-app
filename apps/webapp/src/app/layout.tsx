f686110f349ade7a4baea92d73071b3648cca37e
conradkoh <conradkoh@gmail.com>
2026-05-10 08:21:40 +0800

    fix: move react-grab script from head to body (#43)
    The next/script component should not be placed inside <head>.
    Moving it to the end of <body> avoids hydration mismatch issues.

--- apps/webapp/src/app/layout.tsx
+++ apps/webapp/src/app/layout.tsx
@@ -60,14 +60,6 @@ export default function RootLayout({
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-touch-fullscreen" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        {/* React Grab for development */}
-        {process.env.NODE_ENV === 'development' && (
-          <Script
-            src="//unpkg.com/react-grab/dist/index.global.js"
-            crossOrigin="anonymous"
-            strategy="afterInteractive"
-          />
-        )}
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ConvexClientProvider>
@@ -85,6 +77,14 @@ export default function RootLayout({
           </ConvexQueryCacheProvider>
         </ConvexClientProvider>
         <Toaster />
+        {/* React Grab for development */}
+        {process.env.NODE_ENV === 'development' && (
+          <Script
+            src="//unpkg.com/react-grab/dist/index.global.js"
+            crossOrigin="anonymous"
+            strategy="afterInteractive"
+          />
+        )}
       </body>
     </html>
   );
